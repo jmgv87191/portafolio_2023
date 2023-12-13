39af301d09cddebda9716631e0086401451ba969
juan manuel <jmgv87191@gmail.com>
2023-12-12 20:44:58 -0700

    cambios portafolio

---
+++ js/index.js
@@ -0,0 +1,12 @@
+    document.addEventListener('DOMContentLoaded', function() {
+
+
+        let cont_loader = document.querySelector('.cont_loader');
+
+        setTimeout(() => {
+            
+            cont_loader.classList.add('ocultar')
+        }, 2000);
+
+
+    });
