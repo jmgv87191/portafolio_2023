aec47e4fc1959548060deeead8125b91643129c6
jmgv87191 <jmgv87191@gmail.com>
2023-12-22 20:47:40 -0700

    mas cambios

--- js/index.js
+++ js/index.js
@@ -17,27 +17,32 @@ var typed = new Typed(".typing",{
 
 function cambiarColor(elemento){
 
+    
     for (let i = 0; i < elementos_navegacion.length; i++) {
         elementos_navegacion[i].classList.remove("active")
+        
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/index.html#home") {
+    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#home") {
         home.classList.remove("hidden")
         about.classList.add("hidden")
         services.classList.add("hidden")
         portfolio.classList.add("hidden")
         contact.classList.add("hidden")
+
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/index.html#about") {
+    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#about") {
         home.classList.add("hidden")
         about.classList.remove("hidden")
         services.classList.add("hidden")
         portfolio.classList.add("hidden")
         contact.classList.add("hidden")
+        console.log(about)
+
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/index.html#services") {
+    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#services") {
         home.classList.add("hidden")
         about.classList.add("hidden")
         services.classList.remove("hidden")
@@ -45,7 +50,7 @@ function cambiarColor(elemento){
         contact.classList.add("hidden")
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/index.html#portfolio") {
+    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#portfolio") {
         home.classList.add("hidden")
         about.classList.add("hidden")
         services.classList.add("hidden")
@@ -53,15 +58,13 @@ function cambiarColor(elemento){
         contact.classList.add("hidden")
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/index.html#contact") {
+    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#contact") {
         home.classList.add("hidden")
         about.classList.add("hidden")
         services.classList.add("hidden")
         portfolio.classList.add("hidden")
         contact.classList.remove("hidden")
     }
-
-
 
     elemento.classList.add("active")
 
