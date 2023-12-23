96dff8d8ec398d5b5fbb817392c1ea3946ee4364
jmgv87191 <jmgv87191@gmail.com>
2023-12-22 22:36:47 -0700

    cambios

--- js/index.js
+++ js/index.js
@@ -1,71 +1,29 @@
-let elementos_navegacion = document.querySelectorAll('.nav li a')
-let home = document.querySelector('#home')
-let about = document.querySelector('#about')
-let services = document.querySelector('#services')
-let portfolio = document.querySelector('#portfolio')
-let contact = document.querySelector('#contact')
+let elementos_navegacion = document.querySelectorAll('.nav li a');
+let sections = {
+    home: document.querySelector('#home'),
+    about: document.querySelector('#about'),
+    services: document.querySelector('#services'),
+    portfolio: document.querySelector('#portfolio'),
+    contact: document.querySelector('#contact')
+};
 
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developer", "juan", "manuel"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
 
-
-var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","juan","manuel"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
-
-
-function cambiarColor(elemento){
-
-    
-    for (let i = 0; i < elementos_navegacion.length; i++) {
-        elementos_navegacion[i].classList.remove("active")
-        
+function cambiarColor(elemento) {
+    for (let el of elementos_navegacion) {
+        el.classList.remove("active");
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#home") {
-        home.classList.remove("hidden")
-        about.classList.add("hidden")
-        services.classList.add("hidden")
-        portfolio.classList.add("hidden")
-        contact.classList.add("hidden")
-
+    for (let section in sections) {
+        sections[section].classList.add("hidden");
     }
 
-    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#about") {
-        home.classList.add("hidden")
-        about.classList.remove("hidden")
-        services.classList.add("hidden")
-        portfolio.classList.add("hidden")
-        contact.classList.add("hidden")
-        console.log(about)
-
-    }
-
-    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#services") {
-        home.classList.add("hidden")
-        about.classList.add("hidden")
-        services.classList.remove("hidden")
-        portfolio.classList.add("hidden")
-        contact.classList.add("hidden")
-    }
-
-    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#portfolio") {
-        home.classList.add("hidden")
-        about.classList.add("hidden")
-        services.classList.add("hidden")
-        portfolio.classList.remove("hidden")
-        contact.classList.add("hidden")
-    }
-
-    if (elemento.href === "http://127.0.0.1:5500/portafolio_2023/index.html#contact") {
-        home.classList.add("hidden")
-        about.classList.add("hidden")
-        services.classList.add("hidden")
-        portfolio.classList.add("hidden")
-        contact.classList.remove("hidden")
-    }
-
-    elemento.classList.add("active")
-
+    let sectionId = elemento.getAttribute("href").substring(1); // Obtener el ID de la sección
+    sections[sectionId].classList.remove("hidden");
+    elemento.classList.add("active");
 }
