e4f212db0ffa002136759fd67d41aecc5d879a36
juan Manuel <jmgv87191@gmail.com>
2024-01-10 08:53:33 -0700

    probando cambios

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ let sections = {
 };
 
 var typed = new Typed(".typing", {
-    strings: ["", "Web Designer", "Web Developer", "juan", "manuel"],
+    strings: ["", "Web Designer", "Web Developer", "juan", "manuel","gutierrez"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
